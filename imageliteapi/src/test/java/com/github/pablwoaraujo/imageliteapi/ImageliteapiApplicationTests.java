8cbb32e63d0e17b8f99c731f5bed45342c9e2c52
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2023-11-17 10:41:24 -0300

    chore: inicializando o projeto spring

---
+++ imageliteapi/src/test/java/com/github/pablwoaraujo/imageliteapi/ImageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pablwoaraujo.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
