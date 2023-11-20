38c51cecad041b0035a2250ffa465c71fab4b5cc
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2023-11-20 17:53:01 -0300

    feat: criando o formulário de pesquisa das imagens

--- imagelite/src/app/gallery/page.tsx
+++ imagelite/src/app/gallery/page.tsx
@@ -8,11 +8,12 @@ import { useState } from "react"
 export default function GalleryPage() {
 
   const useService = useImageService();
-
-  const [images, setImages] = useState<Image[]>([]);
+  const [images, setImages] = useState<Image[]>([])
+  const [query, setQuery] = useState<string>("")
+  const [extension, setExtension] = useState<string>("")
 
   async function searchImages() {
-    const result = await useService.search();
+    const result = await useService.search(query, extension);
     setImages(result);
   }
 
@@ -33,9 +34,30 @@ export default function GalleryPage() {
 
   return (
     <Template>
-      <button onClick={searchImages}>Carregar imagens</button>
-      <section className="grid grid-cols-3 gap-8">
-        {renderImagesCard()}
+      <section className="flex flex-col items-center justify-center my-5">
+        <div className="flex space-x-4">
+          <input type="text"
+            onChange={event => setQuery(event.target.value)}
+            className="border px-3 py-2 rounded-lg text-gray-900" />
+
+          <select onChange={event => setExtension(event.target.value)}
+            className="border px-4 py-2 rounded-lg text-gray-900">
+
+            <option value="">All formats</option>
+            <option value="PNG">PNG</option>
+            <option value="JPEG">JPEG</option>
+            <option value="GIF">GIF</option>
+          </select>
+
+          <button className='bg-blue-500 text-white px-4 py-2 rounded-lg' onClick={searchImages}>Search</button>
+          <button className='bg-yellow-500 text-white px-4 py-2 rounded-lg'>Add New</button>
+        </div>
+      </section>
+
+      <section className="grid grid-cols-4 gap-8">
+        {
+          renderImagesCard()
+        }
       </section>
     </Template>
   )
