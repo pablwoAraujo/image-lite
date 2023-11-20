104789889956f66de151bc2bc18121003fc4041c
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2023-11-20 17:37:42 -0300

    feat: buscando na api e renderizando as imagens

--- imagelite/src/app/gallery/page.tsx
+++ imagelite/src/app/gallery/page.tsx
@@ -1,13 +1,41 @@
+"use client"
+
 import { Template, ImageCard } from "@/components"
+import { Image } from "@/resources/image/image.resource";
+import { useImageService } from "@/resources/image/image.service";
+import { useState } from "react"
 
 export default function GalleryPage() {
+
+  const useService = useImageService();
+
+  const [images, setImages] = useState<Image[]>([]);
+
+  async function searchImages() {
+    const result = await useService.search();
+    setImages(result);
+  }
+
+  function renderImageCard(image: Image) {
+    return (
+      <ImageCard
+        key={image.url}
+        title={image.name}
+        src={image.url}
+        size={image.size}
+        dataUpload={image.uploadDate} />
+    )
+  }
+
+  function renderImagesCard() {
+    return images.map(renderImageCard);
+  }
+
   return (
     <Template>
+      <button onClick={searchImages}>Carregar imagens</button>
       <section className="grid grid-cols-3 gap-8">
-        <ImageCard title="Montanha" size="10MB" dataUpload="31/10/2023" src="http://localhost:8080/v1/images/2c695250-e3d0-45e4-a66b-785fba3987b4"></ImageCard>
-        <ImageCard src="http://localhost:8080/v1/images/2c695250-e3d0-45e4-a66b-785fba3987b4"></ImageCard>
-        <ImageCard src="http://localhost:8080/v1/images/2c695250-e3d0-45e4-a66b-785fba3987b4"></ImageCard>
-        <ImageCard src="http://localhost:8080/v1/images/2c695250-e3d0-45e4-a66b-785fba3987b4"></ImageCard>
+        {renderImagesCard()}
       </section>
     </Template>
   )
--- imageliteapi/src/main/java/com/github/pablwoaraujo/imageliteapi/application/images/ImagesController.java
+++ imageliteapi/src/main/java/com/github/pablwoaraujo/imageliteapi/application/images/ImagesController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/v1/images")
 @Slf4j
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ImagesController {
 
     private final ImageService service;
