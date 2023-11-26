879938c42964a20435acbcfbc4a9b7e2b4588797
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2023-11-26 11:27:04 -0300

    feat: implementando a geração do token JWT

--- imageliteapi/src/main/java/com/github/pablwoaraujo/imageliteapi/application/jwt/JwtService.java
+++ imageliteapi/src/main/java/com/github/pablwoaraujo/imageliteapi/application/jwt/JwtService.java
@@ -1,15 +1,55 @@
 package com.github.pablwoaraujo.imageliteapi.application.jwt;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+
 import org.springframework.stereotype.Service;
 
 import com.github.pablwoaraujo.imageliteapi.domain.AccessToken;
 import com.github.pablwoaraujo.imageliteapi.domain.entity.User;
 
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class JwtService {
 
+    private final SecretKeyGenerator keyGenerator;
+
     public AccessToken generateToken(User user) {
-        return new AccessToken(""); // TODO
+        SecretKey key = keyGenerator.getKey();
+        Date expirationDate = generateExpirationDate();
+        Map<String, Object> claims = generateTokenClaims(user);
+
+        String token = Jwts
+                .builder()
+                .signWith(key)
+                .subject(user.getEmail())
+                .expiration(expirationDate)
+                .claims(claims)
+                .compact();
+
+        return new AccessToken(token);
+    }
+
+    private Date generateExpirationDate() {
+        int expirationMinutes = 60;
+        LocalDateTime now = LocalDateTime.now().plusMinutes(expirationMinutes);
+
+        return Date.from(now.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    private Map<String, Object> generateTokenClaims(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("name", user.getName());
+
+        return claims;
     }
 
 }
---
+++ imageliteapi/src/main/java/com/github/pablwoaraujo/imageliteapi/application/jwt/SecretKeyGenerator.java
@@ -0,0 +1,21 @@
+package com.github.pablwoaraujo.imageliteapi.application.jwt;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class SecretKeyGenerator {
+
+    private SecretKey key;
+
+    public SecretKey getKey() {
+        if (key == null) {
+            key = Jwts.SIG.HS256.key().build();
+        }
+
+        return key;
+    }
+}
