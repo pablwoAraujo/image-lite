ba6ebb120d62fb206a992af41bf584673ccc80db
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2023-11-28 12:15:23 -0300

    feat: enviando o token jwt via frontend

--- imagelite/src/resources/image/image.service.ts
+++ imagelite/src/resources/image/image.service.ts
@@ -1,19 +1,32 @@
+import { useAuth } from "..";
 import { Image } from "./image.resource";
 
 class ImageService {
   baseURL: string = "http://localhost:8080/v1/images";
+  auth = useAuth();
 
   async search(query: string = "", extension: string = ""): Promise<Image[]> {
+    const userSession = this.auth.getUserSession();
+
     const url = `${this.baseURL}?query=${query}&extension=${extension}`;
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      headers: {
+        "Authorization": `Bearer ${userSession?.accesToken}`
+      }
+    });
 
     return await response.json();
   }
 
   async save(data: FormData): Promise<string> {
+    const userSession = this.auth.getUserSession();
+
     const response = await fetch(this.baseURL, {
       method: 'POST',
       body: data,
+      headers: {
+        "Authorization": `Bearer ${userSession?.accesToken}`
+      }
     })
 
     return response.headers.get('location') ?? ''
