b13b433dbe5e6be115f22416663f7a9b2d6c7f9e
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2023-11-29 14:47:22 -0300

    feat: definindo a home page

--- imagelite/src/app/page.tsx
+++ imagelite/src/app/page.tsx
@@ -1,5 +1,18 @@
+"use client"
+
+import { useAuth } from "@/resources"
+import LoginPage from "./login/page";
+import GalleryPage from "./gallery/page";
+
 export default function Home() {
+  const auth = useAuth();
+
+  if (!auth.getUserSession()) {
+    return (
+      <LoginPage />
+    )
+  }
   return (
-    <h1>Hello World!</h1>
+    <GalleryPage />
   )
 }
